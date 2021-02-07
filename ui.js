d002f8a314585722b50c306b0b97e7f1e72a6173
Nikhil R <osregularcoder@gmail.com>
2021-02-07 20:49:57 +0530

    Nikhil: Implemented basic box-breathing for 10s interval

--- ui.js
+++ ui.js
@@ -1,11 +1,36 @@
 'use strict';
 const React = require('react');
-const {Text} = require('ink');
+const { Text } = require('ink');
 
-const App = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
-);
+const App = () => {
+	const durationInSeconds = 10;
+	const breathingStates = ['Breathe in', 'Hold', 'Breathe out']
+	const [current, setCurrent] = React.useState(0);
+	const [timer, setTimer] = React.useState(durationInSeconds);
+
+	React.useEffect(() => {
+		const timerId = setInterval(() => {
+			const newValue = timer - 1;
+			setTimer(newValue || durationInSeconds);
+
+			if(newValue == 0) {
+				setCurrent((current + 1) % 3);
+			}
+		}, 1000);
+
+		return () => clearInterval(timerId);
+	});
+
+	return (
+		<>
+			<Text>
+				{timer}
+			</Text>
+			<Text>
+				{breathingStates[current]}
+			</Text>
+		</>
+	);
+};
 
 module.exports = App;
